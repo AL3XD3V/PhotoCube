a59554fc107ad40fce9299bbd7bb862a7d81e2ef
AL3XD3V <real_slam@mail.ru>
2018-10-10 16:41:18 +0300

    initial commit

---
+++ src/main/java/ru/lexeder/App.java
@@ -0,0 +1,13 @@
+package ru.lexeder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args ) throws InterruptedException {
+        System.out.println( "Hello World!" );
+        Thread.sleep(3000);
+    }
+}
